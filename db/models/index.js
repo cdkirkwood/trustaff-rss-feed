aea6b47eef93b38e00c4d7e3af267e66c1f7338a
Connor Kirkwood <cdkirkwood@gmail.com>
2018-11-01 16:47:31 -0400

    definig nurses table

---
+++ db/models/index.js
@@ -0,0 +1,3 @@
+const Nurse = require('./nuse')
+
+module.exports = { Nurse }
---
+++ db/models/nuse.js
@@ -0,0 +1,35 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Nurse = db.define('nurse', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  specialty: {
+    type: Sequelize.STRING
+  },
+  city: {
+    type: Sequelize.STRING
+  },
+  zip: {
+    type: Sequelize.INTEGER
+  },
+  distance: {
+    type: Sequelize.INTEGER
+  },
+  shiftType: {
+    type: Sequelize.STRING
+  },
+  startDate: {
+    type: Sequelize.STRING
+  },
+  rate: {
+    type: Sequelize.INTEGER
+  },
+  notes: {
+    type: Sequelize.STRING
+  }
+})
+
+module.exports = Nurse
