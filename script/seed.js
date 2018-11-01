a613c5cb5a1f9bfccf7b632673fb7eeb20854083
Connor Kirkwood <cdkirkwood@gmail.com>
2018-11-01 17:28:50 -0400

    adding seed file

--- script/seed.js
+++ script/seed.js
@@ -1,24 +1,46 @@
 const XLSX = require('xlsx')
+const db = require('../db')
+const { Nurse }  = require('../db/models')
 
-const testExcelDoc = () => {
-  const sheet = XLSX.readFile('./Pipeline.xlsx').Sheets.Sheet1
-  const rows = +sheet['!ref'].slice(4)
-  const nurses = []
-  for (let i = 2; i <= rows; i++) {
-    const nurse = {}
-    nurse.name = sheet['A' + i].v
-    nurse.specialty = sheet['B' + i].v
-    nurse.city = sheet['C' + i].v
-    nurse.state = sheet['D' + i].v
-    nurse.zip = sheet['E' + i].v
-    nurse.distance = sheet['F' + i].v
-    nurse.shiftType = sheet['G' + i].v
-    nurse.startDate = sheet['H' + i].v
-    nurse.rate = sheet['I' + i].v
-    nurse.notes = sheet['J' + i] ? sheet['J' + i].v : null
-    nurses.push(nurse)
+class NurseClass {
+  constructor(rowNum, sheet) {
+    this.name = sheet['A' + rowNum].v
+    this.specialty = sheet['B' + rowNum].v
+    this.city = sheet['C' + rowNum].v
+    this.state = sheet['D' + rowNum].v
+    this.zip = sheet['E' + rowNum].v
+    this.distance = sheet['F' + rowNum].v
+    this.shiftType = sheet['G' + rowNum].v
+    this.startDate = sheet['H' + rowNum].v
+    this.rate = sheet['I' + rowNum].v
+    this.notes = sheet['J' + rowNum] ? sheet['J' + rowNum].v : null
   }
-  console.log(nurses)
 }
 
-testExcelDoc()
+const seed = async () => {
+  await db.sync({ force: true })
+  console.log('db synced!')
+  const sheet = XLSX.readFile('./Pipeline.xlsx').Sheets.Sheet1
+  const rows = +sheet['!ref'].slice(4)
+  let chain = Promise.resolve()
+  for (let i = 2; i <= rows; i++) {
+    const nurse = new NurseClass(i, sheet)
+    chain = chain.then(() => {
+      return Nurse.create(nurse)
+        .then(() => 'nothing')
+    })
+  }
+  return chain.then(() => {
+    console.log('seed complete')
+  })
+}
+
+seed()
+  .then(() => {
+    db.close()
+    console.log('db connection closed')
+  })
+  .catch(err => {
+    console.error(err.message)
+    process.exitCode = 1
+  })
