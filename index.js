6cb731ef962840cdeedb74fc57d4d14755734606
Connor Kirkwood <cdkirkwood@gmail.com>
2018-11-01 18:20:40 -0400

    working on seeding rss feed data

--- index.js
+++ index.js
@@ -22,15 +22,3 @@ db.sync()
     app.listen(PORT, () => (console.log('Server is alive!')))
   })
 
-// const testRssLink = async() => {
-//   const response = await axios.get('http://rss.trustaff.com')
-//   parseString(response.data, (err, result) => {
-//     if (err) console.error(err)
-//     console.log(result.rss.channel[0].item[157])
-//   })
-// }
-
-// testRssLink()
-//   .then((data) => console.log('done'))
-//   .catch((error) => console.log(error))
-
---
+++ rss-feed.js
@@ -0,0 +1,14 @@
+const axios = require('axios')
+const parseString = require('xml2js').parseString
+
+const testRssLink = async() => {
+  const response = await axios.get('http://rss.trustaff.com')
+  parseString(response.data, (err, result) => {
+    if (err) console.error(err)
+    console.log(result.rss.channel[0].item[157])
+  })
+}
+
+testRssLink()
+  .then((data) => console.log('done'))
+  .catch((error) => console.log(error))
