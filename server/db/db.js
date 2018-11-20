64a8029b014e48c6ee69ed63d68cf6a8a4defc21
Connor Kirkwood <cdkirkwood@gmail.com>
2018-11-20 17:29:36 -0500

    added database URL to db config

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 
 const db = new Sequelize(
-  'postgres://localhost:5432/job-matcher',
+  process.env.DATABASE_URL || 'postgres://localhost:5432/job-matcher',
   {
     logging: false,
     operatorsAliases: false
